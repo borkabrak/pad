b303cedfe16663de1f9bf0730c5108f585b2e347
Jonathan Carter <borkabrak@gmail.com>
2013-08-30 13:55:51 -0500

    Intial commit.
    This is a fresh Rails install.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pad::Application.config.secret_token = 'e72389deb39132155c20387f84d1fa38a263be50bba5d903dcc239e1e7dfad8cd82b472660488bc56690f5b090005330bf351528884d8889ff6e84d36eabf102'
