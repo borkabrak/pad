a37da5a5ff57f53e932d9b0df072bec00bea8975
Jonathan Carter <borkabrak@gmail.com>
2013-08-30 15:15:13 -0500

    Add Client management
    Scaffolding run -- CRUD operations work for Clients, and a basic interface is
    established.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Pad::Application.routes.draw do
+  resources :clients
+
+  get "home/index"
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
